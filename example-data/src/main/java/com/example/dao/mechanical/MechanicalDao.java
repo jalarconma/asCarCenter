a2a3a68411bef42ee69abaa4b86758c82484f78a
Julian Alarcon <juaalarconma@gmail.com>
2019-06-13 21:52:41 -0500

    add assign mechanical query

--- example-data/src/main/java/com/example/dao/mechanical/MechanicalDao.java
+++ example-data/src/main/java/com/example/dao/mechanical/MechanicalDao.java
@@ -13,7 +13,16 @@ import com.example.entities.mechanical.MechanicalId;
 @Repository(value = "BillingDao")
 public interface MechanicalDao extends JpaRepository<Mechanical, MechanicalId> {
 	
-	@Query(value = "SELECT * FROM MECANICOS WHERE ESTADO = 1 AND ROWNUM <= 10", nativeQuery = true)
+	@Query(value = "SELECT * from mecanicos m where ( m.documento, m.tipo_documento) IN (\r\n" + 
+			"\r\n" + 
+			"    SELECT documento, tipo_documento FROM (    \r\n" + 
+			"        SELECT mec.documento, mec.tipo_documento, SUM(sxm.tiempo_estimado) as suma_tiempos FROM mecanicos mec \r\n" + 
+			"        LEFT JOIN mantenimientos mn ON mn.mec_documento = mec.documento AND mn.mec_tipo_documento = mec.tipo_documento\r\n" + 
+			"        LEFT JOIN SERVICIOS_X_MANTENIMIENTOS sxm ON (sxm.cod_mantenimiento = mn.codigo)\r\n" + 
+			"        WHERE mec.estado = 1\r\n" + 
+			"        GROUP BY mec.documento, mec.tipo_documento ORDER BY SUM(sxm.tiempo_estimado) ASC\r\n" + 
+			"    ) sd\r\n" + 
+			") AND ROWNUM <= 10", nativeQuery = true)
 	List<Mechanical> findByPriorityAssign(@Param("year") Integer year, @Param("month") Integer mont);
 
 }
